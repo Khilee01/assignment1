102d8bd446d8a46c123a35cb342f65285a66d38f
Khilee01 <100354343+Khilee01@users.noreply.github.com>
2022-02-24 18:32:55 +0530

    Khilee .cpp

---
+++ Khilee ass1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"size of int: "<< size of (int)<<"byte"<<"\n";
+cout<<"size of float: "<< size of (float)<<"byte"<<"\n";
+cout<<"size of double: "<< size of (double)<<"byte"<<"\n";
+cout<<"size of char: "<< size of (char)<<"byte"<<"\n";
+return 0;
+}
